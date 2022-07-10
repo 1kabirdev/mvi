ea2eb15069be19b24f1b7a0c96b3e7e30a801668
kabir <kabiragamirzaewv@gmail.com>
2022-07-10 17:58:11 +0300

    init commit --- main state and build

--- app/src/main/java/com/mvi/ui/viewstate/MainState.kt
+++ app/src/main/java/com/mvi/ui/viewstate/MainState.kt
@@ -3,7 +3,6 @@ package com.mvi.ui.viewstate
 import com.mvi.data.User
 
 sealed class MainState {
-
     object Idle : MainState()
     object Loading : MainState()
     data class Users(val user: List<User>) : MainState()
