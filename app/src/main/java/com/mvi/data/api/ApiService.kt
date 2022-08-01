1ecd2953c4ab14a491d5993bb394fa07a902292e
kabir <kabiragamirzaewv@gmail.com>
2022-08-01 17:47:23 +0300

    commit --- refactor code api 01.08.2022

--- app/src/main/java/com/mvi/data/api/ApiService.kt
+++ app/src/main/java/com/mvi/data/api/ApiService.kt
@@ -4,10 +4,16 @@ import com.mvi.data.models.User
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("users")
     suspend fun getUsers(): List<User>
+
+    @GET("users/{login}")
+    suspend fun getUser(
+        @Path("login") login: String
+    ): User
 
     object RetrofitBuilder {
         private const val BASE_URL = " https://api.github.com/"
