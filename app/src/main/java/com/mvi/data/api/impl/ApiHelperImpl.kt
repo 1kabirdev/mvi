c2da98fe078f513655cfef0cae8c4288fdad52b1
kabir <kabiragamirzaewv@gmail.com>
2022-08-01 18:31:16 +0300

    commit --- refactor code api helper 01.08.2022

--- app/src/main/java/com/mvi/data/api/impl/ApiHelperImpl.kt
+++ app/src/main/java/com/mvi/data/api/impl/ApiHelperImpl.kt
@@ -7,4 +7,8 @@ class ApiHelperImpl(private val apiService: ApiService) : ApiHelper {
     override suspend fun getUsers(): List<User> {
         return apiService.getUsers()
     }
+
+    override suspend fun getUser(login: String): User {
+        return apiService.getUser(login)
+    }
 }
